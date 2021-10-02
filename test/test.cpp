be202e81559c31abec1ce65571265eb5494d32c3
Rahul Karanam <rahulkaranam@wireless-10-104-227-79.umd.edu>
2021-10-01 22:41:07 -0400

    updated test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
-#include "include/PID.hpp"
+#include "../include/PID.hpp"
 
 
 TEST(pid,compute){
     PID pid(0.1,0.2,0.005,0.01);
     EXPECT_EQ(-0.602, pid.computePID(51,52));
     EXPECT_EQ(0.903,pid.computePID(14,13));
-    EXPEPT_EQ(-0.4515,pid.computePID(10,10)); 
+    EXPECT_EQ(-0.4515,pid.computePID(10,10)); 
 }
